06030a89bac6432867c23a1fa5d8e580cfceae78
hatsu38 <hajiwata0308@gmail.com>
2021-09-16 14:37:36 +0900

    rails g devise_token_auth:install EmployeeAuthentication authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'EmployeeAuthentication', at: 'authenticate'
   if Rails.env.development?
     post "/graphql", to: "graphql#execute"
   end
